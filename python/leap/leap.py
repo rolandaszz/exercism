4e159bfbe350d2314c4ad3c2b6f444ff6830f25b
Rolandas Zikis Ziukevicius <rolandas@Rolandass-MacBook-Pro.local>
2018-10-09 14:05:53 +0300

    removed: input validation, int conversion, SESE; rearrange tests by most likely

--- python/leap/leap.py
+++ python/leap/leap.py
@@ -1,31 +1,18 @@
-import datetime
-
-#def is_leap_year(year):
+#removed: input validation, int conversion, SESE; rearrange tests by most likely
 
 def is_leap_year(year):
-    isleap = False
-
-    #validate year
-    #https://www.tutorialspoint.com/How-to-do-date-validation-in-Python
-
-    yearf = '%Y'
-
-    try:
-        date_check = datetime.datetime.strptime(str(year), yearf)
-        print('Entry:', date_check.year)
-    except ValueError:
-        print('Incorrect year format! Accepted values are [0001, 9999]')
 
     #on every year that is evenly divisible by 4
     #except every year that is evenly divisible by 100
     #unless the year is also evenly divisible by 400
-    if int(year) >= 400 and int(year) % 400 == 0:
-        isleap = True
-    elif int(year) >= 100 and int(year) % 100 == 0:
-        isleap = False
-    elif int(year) % 4 == 0:
-        isleap = True
-    else:
-        isleap = False
 
-    return isleap
+    if year % 4 != 0:
+        return False
+
+    elif year % 100 != 0:
+        return True
+
+    elif year % 400 == 0:
+        return True
+
+    return False
